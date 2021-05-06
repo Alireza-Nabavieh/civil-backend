f90fa89b4ff3f20ae242f83cfee618cbb2ecfad4
a.nabavieh <nabavieh@gmail.com>
2021-05-07 01:34:15 +0430

    change databse

--- config/database.js
+++ config/database.js
@@ -16,35 +16,6 @@
 
 
 
-// module.exports = ({ env }) => ({
-//   defaultConnection: 'default',
-//   connections: {
-//     default: {
-//       connector: 'bookshelf',
-//       settings: {
-//         client: 'postgres',
-//         host: env('DATABASE_HOST', 'localhost'),
-//         port: env.int('DATABASE_PORT', 5432),
-//         database: env('DATABASE_NAME', 'purchase'),
-//         username: env('DATABASE_USERNAME', 'postgres'),
-//         password: env('DATABASE_PASSWORD', 'Ali87243210115'),
-//         schema: env('DATABASE_SCHEMA', 'public'), // Not Required
-//         // ssl: {
-//         //   rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
-//         // },
-//       },
-//       options: {
-//         ssl: env.bool('DATABASE_SSL', false),
-//       },
-//     },
-//   },
-// });
-
-
-
-const parse = require('pg-connection-string').parse;
-const config = parse(process.env.DATABASE_URL);
-
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
@@ -52,18 +23,47 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: config.host,
-        port: config.port,
-        database: config.database,
-        username: config.user,
-        password: config.password,
-        ssl: {
-          rejectUnauthorized: false,
-        },
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'purchase'),
+        username: env('DATABASE_USERNAME', 'postgres'),
+        password: env('DATABASE_PASSWORD', 'Ali87243210115'),
+        schema: env('DATABASE_SCHEMA', 'public'), // Not Required
+        // ssl: {
+        //   rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+        // },
       },
       options: {
-        ssl: true,
+        ssl: env.bool('DATABASE_SSL', false),
       },
     },
   },
 });
+
+
+
+// const parse = require('pg-connection-string').parse;
+// const config = parse(process.env.DATABASE_URL);
+
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'bookshelf',
+//       settings: {
+//         client: 'postgres',
+//         host: config.host,
+//         port: config.port,
+//         database: config.database,
+//         username: config.user,
+//         password: config.password,
+//         ssl: {
+//           rejectUnauthorized: false,
+//         },
+//       },
+//       options: {
+//         ssl: true,
+//       },
+//     },
+//   },
+// });
